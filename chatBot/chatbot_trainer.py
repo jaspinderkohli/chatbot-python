00961ccbb4188503488b18f505e0c63ebc8528f6
Jaspinder Singh <jaspinderkohli@gmail.com>
2021-12-21 01:33:51 -0330

    chatbot code added and trainer dataset modification

--- chatBot/chatbot_trainer.py
+++ chatBot/chatbot_trainer.py
@@ -36,7 +36,7 @@ words = sorted(set(words))
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))
 
 training = []
 output_empty = [0] * len(classes)
@@ -45,15 +45,36 @@ for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    # print(word_patterns)
     for word in words:
+        # print("---")
+        # print(word)
+        # print("---")
         bag.append(1) if word in word_patterns else bag.append(0)
 
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
-    
 
+# print(training)
+random.shuffle(training)
+training = np.array(training)
 
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
 
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax')) # percentages of how likely it is
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbotmodel.h5', hist)
+print("Done")
 # import nltk
 # nltk.download('punkt')
