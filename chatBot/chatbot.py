54aedc7738c65ae5513e1f692cdd6b6521dbda06
Jaspinder Singh <jaspinderkohli@gmail.com>
2022-01-16 17:31:11 -0330

    restructuring code

--- chatBot/chatbot.py
+++ chatBot/chatbot.py
@@ -14,47 +14,52 @@ words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = load_model('chatbotmodel.h5')
 
-def clean_up_sentense(sentence):
-    sentence_words = nltk.word_tokenize(sentence)
-    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
-    return sentence_words
 
-def bag_of_words(sentence):
-    sentence_words = clean_up_sentense(sentence)
-    bag = [0] * len(words)
-    for w in sentence_words:
-        for i, word in enumerate(words):
-            if word == w:
-                bag[i] = 1
-    return np.array(bag)
+class ChatBot:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def clean_up_sentense(sentence):
+        sentence_words = nltk.word_tokenize(sentence)
+        sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+        return sentence_words
+
+    def bag_of_words(self, sentence):
+        sentence_words = self.clean_up_sentense(sentence)
+        bag = [0] * len(words)
+        for w in sentence_words:
+            for i, word in enumerate(words):
+                if word == w:
+                    bag[i] = 1
+        return np.array(bag)
+
+    def predict_class(self, sentence):
+        bow = self.bag_of_words(sentence)
+        res = model.predict(np.array([bow]))[0]
+        ERROR_THRESHOLD = 0.25  #bellow 25 % dont include
+        results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+        results.sort(key = lambda x: x[1], reverse=True)
+        return_list = []
+        for r in results:
+            return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+        return return_list
+
+    def get_response(self, indents_list, intents_json):
+        tag = indents_list[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                result = random.choice(i['responses'])
+                break
+        return result
 
 
-def predict_class(sentence):
-    bow = bag_of_words(sentence)
-    res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25  #bellow 25 % dont include
-    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
-    results.sort(key = lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-    return return_list
-
-
-def get_response(indents_list, intents_json):
-    tag = indents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
-    return result
-
-print("Bot is running")
-
-while True:
-    message = input("")
-    ints = predict_class(message)
-    res = get_response(ints, intents)
-    print(res)
+if __name__ == '__main__':
+    print("Bot is running")
+    obj = ChatBot()
+    while True:
+        message = input("")
+        ints = obj.predict_class(message)
+        res = obj.get_response(ints, intents)
+        print(res)
